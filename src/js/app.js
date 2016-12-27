3f28dccff87be13245214ac8902f572efbb7cc03
jetpackpony <jetpackpony@gmail.com>
2016-12-27 16:20:35 +0300

    Create a mobile version of the itineraries-list page

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
   $('.modal').modal('show');
+
+  var openDeets = function() {
+    $('body').addClass('noscroll');
+    $('.body-shadow').removeClass('hidden');
+    $('.bottom-single-overlay').removeClass('hidden');
+    $('#itinerary-details').removeClass('hidden');
+  };
+  var closeDeets = function() {
+    $('body').removeClass('noscroll');
+    $('.body-shadow').addClass('hidden');
+    $('.bottom-single-overlay').addClass('hidden');
+    $('#itinerary-details').addClass('hidden');
+    $('#route-details').addClass('hidden');
+  };
+
+  $('.itinerary-short:first').click(function() {
+    openDeets();
+  });
+
+  $('.body-shadow').click(function() {
+    closeDeets();
+  });
+  $('#itinerary-details .close-button').click(function() {
+    closeDeets();
+  });
+
+  $('.route-item').click(function() {
+    $('#route-details').removeClass('hidden');
+  });
+  $('#route-details .close-button').click(function() {
+    $('#route-details').addClass('hidden');
+  });
 });
